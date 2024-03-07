4e319921491cb083845f65681c9042673fbc2a63
collins <chigbo.c.o@gmail.com>
2024-03-07 21:28:19 +0100

    solved word sorting problem

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,35 +1,12 @@
 package org.example;
 
-import org.example.kyu6.HumanReadableTime;
-
-
-import java.time.*;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
+import org.example.kyu6.YourOrderPlease;
 
 public class Main {
-    public static void $(){
-        System.out.println("Hlel");
-    }
-    public static void £(){
-        System.out.println("Hlel");
-    }
-   public static void main(String[] args){
-      /* var stream = List.of("bird-", "bunny-", "cat-", "dog-", "fish-", "lamb-", "mouse-");
-       Spliterator<String> spliterator = stream.spliterator();
-       Spliterator<String> newbag = spliterator.trySplit();
-       System.out.println(newbag.tryAdvance(System.out::println));
-       System.out.println(newbag.tryAdvance(System.out::println));
-       spliterator.forEachRemaining(System.out::println);
-       newbag.forEachRemaining(System.out::println);
-       IntStream.of(1,2,3).parallel().forEach(System.out::println);
-       */
 
+   public static void main(String[] args) {
+       YourOrderPlease y = new YourOrderPlease();
+       System.out.println(y.order("4of Fo1r pe6ople g3ood th5e the2"));
 
-    }
    }
+}
